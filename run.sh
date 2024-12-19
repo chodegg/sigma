700105e59428177ce8ddffaf4486797820e3e9c3
chodegg <sang0374795542@gmail.com>
2024-12-19 15:08:49 +0000

    Thêm file mới và cập nhật mã nguồn

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 java -Xmx6024M -Xms6024M -jar server.jar nogui
+git remote set-url origin https://chodegg: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@github.com/chodegg/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.git
